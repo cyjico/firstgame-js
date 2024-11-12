18b79a14476d09f151cc2b90dc6c7a360d32cf5f
cyjico <going2email2@gmail.com>
2024-11-12 11:54:51 +0800

    fix: createGame outdated

---
+++ public/js/arche/createBullet.js
@@ -0,0 +1,83 @@
+import Polygon2dCollider from '../ecs/comps/polygon2dCollider.js';
+import Sprite from '../ecs/comps/sprite.js';
+import Transform2d from '../ecs/comps/transform2d.js';
+import Sys from '../ecs/core/sys.js';
+import loadImage from '../util/loadImage.js';
+
+export class BulletComp {
+  /**
+   * @param {string} owner
+   * @param {import("../ecs/util/vector2d.js").default} dir
+   * @param {number} spd
+   * @param {import("../ecs/util/vector2d.js").default} pos
+   * @param {number} rot
+   */
+  constructor(owner, dir, spd, pos, rot) {
+    this.owner = owner;
+
+    this.vec = dir.clone().mul(spd);
+    this.pos = pos.clone();
+    this.rot = rot;
+  }
+}
+
+export class BulletSys extends Sys {
+  /**
+   * @param {HTMLCanvasElement} canvas
+   */
+  constructor(canvas) {
+    super();
+    this.canvas = canvas;
+  }
+
+  /** @type {import('../ecs/core/sys.js').SysAction} */
+  update = (ginfo) => {
+    const ents = ginfo.entMger().getEntsWithComp_t(BulletComp);
+
+    for (const ent of ents) {
+      const bcomp = ginfo.entMger().getComp_t(ent, BulletComp);
+      const t2d = ginfo.entMger().getComp_t(ent, Transform2d);
+      if (!bcomp || !t2d) return;
+
+      t2d.pos = bcomp.pos.add(bcomp.vec.clone().mul(ginfo.time.dt * 0.001)).clone();
+      t2d.rot = bcomp.rot;
+
+      if (
+        t2d.pos.x < -50 ||
+        t2d.pos.x > this.canvas.width + 50 ||
+        t2d.pos.y < -50 ||
+        t2d.pos.y > this.canvas.height + 50
+      )
+        ginfo.entMger().destroyEnt(ent);
+    }
+  };
+}
+
+const POLY2DCOL_RECT = Polygon2dCollider.fromRect(40, 25);
+
+/**
+ * @param {import("../ecs/core/entMger.js").default} entMger
+ * @param {string} owner
+ * @param {import("../ecs/util/vector2d.js").default} dir
+ * @param {number} spd
+ * @param {import("../ecs/util/vector2d.js").default} pos
+ * @param {number} rot
+ */
+export default function createBullet(entMger, owner, dir, spd, pos, rot) {
+  const id = entMger.createEnt();
+  const t2d = new Transform2d([0, 0]);
+  let sprite = new Sprite({
+    rot: 0,
+    width: 40,
+    height: 25,
+  });
+  const bcomp = new BulletComp(owner, dir, spd, pos, rot);
+
+  loadImage(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsf53D6qW-r1u5qULvvnESTXHirMs-m6ASJA&s',
+  ).then((img) => {
+    sprite.image = img;
+
+    entMger.addComps(id, POLY2DCOL_RECT, t2d, sprite, bcomp);
+  });
+}
--- public/js/createGame.js
+++ public/js/createGame.js
@@ -1,96 +1,27 @@
-import Polygon2d from './ecs/comps/polygon2d.js';
-import Polygon2dCollider from './ecs/comps/polygon2dCollider.js';
-import Polygon2dMaterial from './ecs/comps/polygon2dMaterial.js';
-import Sprite from './ecs/comps/sprite.js';
-import Transform2d from './ecs/comps/transform2d.js';
+import { BulletSys } from './arche/createBullet.js';
+import createPlayer from './arche/createPlayer.js';
 import GameLoop from './ecs/gameLoop.js';
-import Vector2d from './ecs/util/vector2d.js';
 import InputSys from './ecs/systems/inputSys.js';
 import Polygon2dCollision from './ecs/systems/polygon2dCollision.js';
 import Polygon2dRenderer from './ecs/systems/polygon2dRenderer.js';
 import RendererMaster from './ecs/systems/rendererMaster.js';
 import SpriteRenderer from './ecs/systems/spriteRenderer.js';
-import loadImage from './util/loadImage.js';
 
 /**
- * @param {HTMLCanvasElement}        canvas
+ * @param {HTMLCanvasElement} canvas
  * @param {CanvasRenderingContext2D} ctx2d
  */
 export default async function createGame(canvas, ctx2d) {
-  const inputSystem = new InputSys(canvas);
   const gameLoop = new GameLoop();
-
-  const player = await createUser(gameLoop, inputSystem);
+  const inputSys = new InputSys(canvas);
 
   gameLoop.setSystems([
-    player.system,
+    createPlayer(gameLoop.entMger, inputSys).sys,
+    new BulletSys(canvas),
     new Polygon2dCollision(true),
     new RendererMaster(ctx2d, new Polygon2dRenderer(), new SpriteRenderer()),
-    inputSystem,
+    inputSys,
   ]);
 
   return gameLoop;
-}
-
-/**
- * @param {GameLoop}    gameLoop
- * @param {InputSys} inputSystem
- */
-async function createUser(gameLoop, inputSystem) {
-  const id = gameLoop.entMger.createEnt();
-  const t2d = new Transform2d([50, 50]);
-  const sprite = new Sprite({
-    image: await loadImage(
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/512px-Tux.svg.png',
-    ),
-    width: 50,
-    height: 50,
-  });
-
-  gameLoop.entMger.addComps(
-    id,
-    Polygon2dCollider.fromRect(50, 50),
-    t2d,
-    sprite,
-  );
-
-  const __poly2dRect = Polygon2d.fromRect(50, 50);
-  const __poly2dMat = new Polygon2dMaterial({
-    fillStyle: '#fff',
-  });
-
-  /** @type {import('./ecs/core/sys.js').default} */
-  const system = {
-    start: null,
-    update: (ginfo) => {
-      const fixedDtSec = ginfo.time.fixedDt * 0.001;
-
-      if (inputSystem.isKeyPressed('w', false))
-        t2d.pos.add(Vector2d.fromRadians(t2d.rot).mul(fixedDtSec).mul(250));
-
-      if (inputSystem.isKeyPressed('s', false))
-        t2d.pos.add(
-          Vector2d.fromRadians(t2d.rot).negate().mul(fixedDtSec).mul(250),
-        );
-
-      if (inputSystem.isKeyPressed('a', false)) t2d.rot -= fixedDtSec * 4;
-      if (inputSystem.isKeyPressed('d', false)) t2d.rot += fixedDtSec * 4;
-
-      if (inputSystem.mouse.getButtonClicked('prim'))
-        ginfo
-          .entMger()
-          .addComps(
-            ginfo.entMger().createEnt(),
-            Polygon2dCollider.fromRect(50, 50),
-            new Transform2d([inputSystem.mouse.x, inputSystem.mouse.y]),
-            __poly2dRect,
-            __poly2dMat,
-          );
-    },
-    fixedUpdate: null,
-  };
-
-  return {
-    system,
-  };
 }
