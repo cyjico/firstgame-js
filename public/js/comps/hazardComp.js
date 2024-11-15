5aeed3feb674c323fa7c838bed3582f43f990d6b
cyjico <going2email2@gmail.com>
2024-11-15 21:53:28 +0800

    feat: add tags to HazardComp

--- public/js/comps/hazardComp.js
+++ public/js/comps/hazardComp.js
@@ -1,8 +1,12 @@
 export default class HazardComp {
   /**
    * @param {number} dmg
+   * @param {string} [tag]
+   * @param {string[]} [excludeTags]
    */
-  constructor(dmg) {
+  constructor(dmg, tag = '', excludeTags = []) {
     this.dmg = dmg;
+    this.tag = tag;
+    this.excludeTags = new Set(excludeTags);
   }
 }
