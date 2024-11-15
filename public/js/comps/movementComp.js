4bc1e66169a3069361ac56f8e4cb0c5bf2c0e273
cyjico <going2email2@gmail.com>
2024-11-15 21:52:17 +0800

    refactor: make MovementSys make sense

--- public/js/comps/movementComp.js
+++ public/js/comps/movementComp.js
@@ -4,20 +4,21 @@ export default class MovementComp {
   constructor({
     spd = 0.4,
     smoothness = 0.01,
-    dir = Vector2d.zero,
+    dir: dirDelta = Vector2d.zero,
     targetDir = Vector2d.zero,
     rotSpeed = 0.004,
-    rotSmoothness = 0.02,
-    rot = 0,
+    rotSmoothness = 0.04,
+    rot: rotDelta = 0,
     targetRot = 0,
   } = {}) {
     this.spd = spd;
     this.smoothness = smoothness;
-    this.dir = dir;
     this.targetDir = targetDir;
+    this._dirDelta = dirDelta;
+
     this.rotSpeed = rotSpeed;
     this.rotSmoothness = rotSmoothness;
-    this.rot = rot;
     this.targetRot = targetRot;
+    this._rotDelta = rotDelta;
   }
 }
