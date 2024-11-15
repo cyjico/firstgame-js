2ccfa9ba6f47dfde5dc4d7da0c1a9bf131598cbb
cyjico <going2email2@gmail.com>
2024-11-15 21:53:36 +0800

    feat: make HazardComp apply both ways

--- public/js/systems/hazardSys.js
+++ public/js/systems/hazardSys.js
@@ -1,6 +1,5 @@
 import HazardComp from '../comps/hazardComp.js';
 import HealthComp from '../comps/healthComp.js';
-import Polygon2dCollider from '../ecs/comps/polygon2dCollider.js';
 import Sys from '../ecs/core/sys.js';
 
 export default class HazardSys extends Sys {
@@ -10,46 +9,28 @@ export default class HazardSys extends Sys {
       'collision_enter',
       /** @type {import('../ecs/systems/polygon2dCollision.js').CollisionEventListener} */
       ({ ent1, ent2 }) => {
-        const $ = this.#structurizeEntities(entMger, ent1, ent2);
-        if (!$) return;
-
-        $.other.health.curHp -= $.self.hazard.dmg;
-        if ($.other.health.curHp <= 0) entMger.destroyEnt($.other_id);
+        this.#applyHazard(entMger, ent1, ent2);
+        this.#applyHazard(entMger, ent2, ent1);
       },
     );
   };
 
   /**
    * @param {import('../ecs/core/entMger.js').default} entMger
-   * @param {number} ent1
-   * @param {number} ent2
+   * @param {number} selfId Entity applying the hazard.
+   * @param {number} otherId Entity receiving the hazard.
    */
-  #structurizeEntities(entMger, ent1, ent2) {
-    let self = ent1;
-    let other = ent2;
-    let hazard = entMger.getComp_t(self, HazardComp);
-    if (!hazard) {
-      self = ent2;
-      other = ent1;
+  #applyHazard(entMger, selfId, otherId) {
+    const hazard = entMger.getComp_t(selfId, HazardComp);
+    const other_health = entMger.getComp_t(otherId, HealthComp);
+    if (!hazard || !other_health) return;
 
-      hazard = entMger.getComp_t(self, HazardComp);
-      if (!hazard) return null;
+    const other_hazard = entMger.getComp_t(otherId, HazardComp);
+    if (other_hazard && hazard.excludeTags.has(other_hazard.tag)) return;
+
+    other_health.curHp -= hazard.dmg;
+    if (other_health.curHp <= 0) {
+      entMger.destroyEnt(otherId);
     }
-
-    const poly2dCol = entMger.getComp_t(self, Polygon2dCollider);
-    const other_health = entMger.getComp_t(other, HealthComp);
-    if (!poly2dCol || !other_health) return null;
-
-    return {
-      self_id: self,
-      self: {
-        hazard,
-        poly2dCol,
-      },
-      other_id: other,
-      other: {
-        health: other_health,
-      },
-    };
   }
 }
