9a757835ef8a7d77eb16988a838dc73795b466f1
cyjico <going2email2@gmail.com>
2024-11-15 21:55:49 +0800

    refactor: update player.js to reflect changes

--- public/js/arche/player.js
+++ public/js/arche/player.js
@@ -1,6 +1,4 @@
-import Polygon2d from '../ecs/comps/polygon2d.js';
 import Polygon2dCollider from '../ecs/comps/polygon2dCollider.js';
-import Polygon2dMaterial from '../ecs/comps/polygon2dMaterial.js';
 import Transform2d from '../ecs/comps/transform2d.js';
 import Sys from '../ecs/core/sys.js';
 import inputHandler, { MOUSE_BUTTON } from '../ecs/systems/inputHandler.js';
@@ -12,19 +10,28 @@ import Sprite from '../ecs/comps/sprite.js';
 import { CollisionLayer } from '../constants.js';
 import HealthComp from '../comps/healthComp.js';
 import createRangedWeapon from '../comps/invComp.createRangedWeapon.js';
+import { createChaser } from './enemy/chaser.js';
 
-class PlayerComp {}
+export class PlayerComp {}
 
 export class PlayerSys extends Sys {
   /**
    * @type {import("../ecs/core/sys.js").SysAction} ginfo
    */
-  update = (ginfo) => {
-    const ent = ginfo.entMger.getEntsWithComp_t(PlayerComp).next().value;
+  update = ({ time, entMger }) => {
+    // DEBUGGING
+    if (inputHandler.mouse.buttons.down & MOUSE_BUTTON.PRIMARY) {
+      createChaser(entMger, [
+        inputHandler.mouse.pos.x,
+        inputHandler.mouse.pos.y,
+      ], -Math.PI + Math.random() * Math.PI * 2);
+    }
+
+    const ent = entMger.getEntsWithComp_t(PlayerComp).next().value;
     if (ent == null) return;
 
-    const t2d = ginfo.entMger.getComp_t(ent, Transform2d);
-    const movComp = ginfo.entMger.getComp_t(ent, MovementComp);
+    const t2d = entMger.getComp_t(ent, Transform2d);
+    const movComp = entMger.getComp_t(ent, MovementComp);
     if (!t2d || !movComp) return;
 
     movComp.targetDir = Vector2d.zero;
@@ -34,38 +41,17 @@ export class PlayerSys extends Sys {
     if (inputHandler.isKeyPressed('s'))
       movComp.targetDir = Vector2d.fromRadians(t2d.rot + Math.PI);
 
-    movComp.targetRot = 0;
-
-    if (inputHandler.isKeyPressed('d')) movComp.targetRot = 1;
-    if (inputHandler.isKeyPressed('a')) movComp.targetRot = -1;
+    if (inputHandler.isKeyPressed('d')) movComp.targetRot += 0.06;
+    if (inputHandler.isKeyPressed('a')) movComp.targetRot -= 0.06;
 
     if (inputHandler.keys.down.has('x')) {
-      const invComp = ginfo.entMger.getComp_t(ent, InvComp);
+      const invComp = entMger.getComp_t(ent, InvComp);
 
       if (
         invComp &&
-        invComp.items[invComp.curItemIdx]?.canUse(
-          ginfo.time.t,
-          ginfo.entMger,
-          ent,
-        )
+        invComp.items[invComp.curItemIdx]?.canUse(time.t, entMger, ent)
       )
-        invComp.items[invComp.curItemIdx].use(ginfo.time.t, ginfo.entMger, ent);
-    }
-
-    // DEBUGGING
-    if (inputHandler.mouse.buttons.down & MOUSE_BUTTON.PRIMARY) {
-      ginfo.entMger.addComps(
-        ginfo.entMger.createEnt(),
-        new Transform2d([inputHandler.mouse.pos.x, inputHandler.mouse.pos.y]),
-        Polygon2d.fromRect(25, 25),
-        Polygon2dCollider.fromRect(25, 25),
-        new Polygon2dMaterial({
-          lineWidth: 2,
-          fillStyle: '#f5bd0d',
-        }),
-        new HealthComp(50),
-      );
+        invComp.items[invComp.curItemIdx].use(time.t, entMger, ent);
     }
   };
 }
@@ -94,13 +80,15 @@ export async function createPlayer(entMger, pos) {
       width: 28,
       height: 25,
     }),
-    new MovementComp(),
+    new MovementComp({
+      targetRot: -Math.PI / 2,
+    }),
     new InvComp([
       createRangedWeapon({
         name: 'default',
         range: 10,
         projSpd: 1,
-        projDmg: 10,
+        projDmg: 50,
         projCount: 1,
         projSprite: new Sprite({
           img: await loadImage(
