7683ea49981d3ad411c76e5645fc498a9197c05c
cyjico <going2email2@gmail.com>
2024-11-15 18:03:49 +0800

    feat: add multiple collision handling & event driven collision

--- public/js/ecs/systems/polygon2dCollision.js
+++ public/js/ecs/systems/polygon2dCollision.js
@@ -9,19 +9,27 @@ import {
 } from './polygon2dCollision.util.js';
 
 /**
+ * @typedef {(data: {
+ *   mtv: import('../util/vector2d.js').default,
+ *   ent1: number,
+ *   ent2: number
+ * }) => void} CollisionEventListener
+ */
+
+/**
  * @param {number} prevState
  * @param {boolean} isColliding
  */
 function getNewState(prevState, isColliding) {
   if (isColliding) {
-    return prevState & CollisionState.NONE
-      ? CollisionState.ENTER
-      : CollisionState.STAY;
+    if (prevState === CollisionState.NONE) return CollisionState.ENTER;
+
+    return CollisionState.STAY;
   }
 
-  return prevState & CollisionState.STAY
-    ? CollisionState.EXIT
-    : CollisionState.NONE;
+  if (prevState === CollisionState.STAY) return CollisionState.EXIT;
+
+  return CollisionState.NONE;
 }
 
 export default class Polygon2dCollision extends Sys {
@@ -34,23 +42,14 @@ export default class Polygon2dCollision extends Sys {
   /**
    * @type {import('../core/sys.js').SysAction}
    */
-  fixedUpdate = ({ entMger }) => {
+  fixedUpdate = ({ entMger, evtBus }) => {
     /**
-     * Current frame cache; resets every frame.
+     * Contains the collisions that were already calculated.
+     * `main_ent_id => other_ent_id`
+     *
+     * @type {Map<number, Set<number>>}
      */
-    const fcache = {
-      /**
-       * Contains the collisions that were already calculated.
-       * `main_ent_id => other_ent_id`
-       *
-       * @type {Map<number, Set<number>>}
-       */
-      col: new Map(),
-      /**
-       * Contains the reset[ted] collision infos.
-       */
-      rst: new Set(),
-    };
+    const wasCalculated = new Map();
 
     for (const ent1 of entMger.getEntsWithComp_t(Polygon2dCollider)) {
       const col1 = entMger.getComp_t(ent1, Polygon2dCollider);
@@ -59,61 +58,84 @@ export default class Polygon2dCollision extends Sys {
       const t1 = entMger.getComp_t(ent1, Transform2d);
       if (!t1) continue;
 
-      // Reset ent1 if ent1 wasn't reset[ted] in the current frame.
-      if (!fcache.rst.has(ent1)) {
-        fcache.rst.add(ent1);
-        col1.info.prevState = col1.info.state;
-        col1.info.state = getNewState(col1.info.prevState, false);
-      }
-
       const nearbyEnts = entMger.getEntsWithComp_t(Polygon2dCollider);
       for (const ent2 of nearbyEnts) {
-        if (ent1 === ent2 || fcache.col.get(ent1)?.has(ent2)) continue;
+        if (ent1 === ent2 || wasCalculated.get(ent1)?.has(ent2)) {
+          continue;
+        }
 
         const col2 = entMger.getComp_t(ent2, Polygon2dCollider);
         const t2 = entMger.getComp_t(ent2, Transform2d);
         if (!col2 || !t2) continue;
 
         // Cache ent1 and ent2 interaction as having been calculated already.
-        if (!fcache.col.get(ent1)?.add(ent2))
-          fcache.col.set(ent1, new Set([ent2]));
-        if (!fcache.col.get(ent2)?.add(ent1))
-          fcache.col.set(ent2, new Set([ent1]));
+        if (!wasCalculated.get(ent1)?.add(ent2))
+          wasCalculated.set(ent1, new Set([ent2]));
+        if (!wasCalculated.get(ent2)?.add(ent1))
+          wasCalculated.set(ent2, new Set([ent1]));
 
-        if (
-          (col1.rules.mask & col2.rules.layer) === 0 &&
-          (col2.rules.mask & col1.rules.layer) === 0
-        )
+        const mtv = this.calcCollision(col1, t1, col2, t2);
+
+        if (!mtv) {
+          col1.trySetInfo(ent2, {
+            mtv: null,
+            other: ent2,
+            state: getNewState(col1.tryGetInfo(ent2).state, false),
+          });
+          col2.trySetInfo(ent1, {
+            mtv: null,
+            other: ent1,
+            state: getNewState(col2.tryGetInfo(ent1).state, false),
+          });
           continue;
+        }
 
-        if (!testBoundsBounds(col1.calcAabb(t1), col2.calcAabb(t2))) continue;
+        const info1 = {
+          mtv: mtv.cpy(),
+          other: ent2,
+          state: getNewState(col1.tryGetInfo(ent2).state, true),
+        };
+        col1.trySetInfo(ent2, info1);
 
-        const tc1 = col1.calcTransformed(t1);
-        const tc2 = col2.calcTransformed(t2);
+        const info2 = {
+          mtv: mtv.cpy().neg(),
+          other: ent1,
+          state: getNewState(col2.tryGetInfo(ent1).state, true),
+        };
+        col2.trySetInfo(ent1, info2);
 
-        const mtv = calcMinTranslationVec(tc1, tc2);
-        if (mtv) {
-          col1.info.state = getNewState(col1.info.prevState, true);
-          col1.info.otherEntId = ent2;
-          col1.info.mtv = mtv;
+        evtBus.emit(`collision_${CollisionState.toString(info1.state)}`, {
+          mtv,
+          ent1,
+          ent2,
+        });
 
-          // Reset ent2 if ent2 wasn't reset[ted] in the current frame.
-          if (!fcache.rst.has(ent2)) {
-            fcache.rst.add(ent2);
-            col2.info.prevState = col2.info.state;
-          }
-          
-          col2.info.state = getNewState(col1.info.prevState, true);
-          col2.info.otherEntId = ent1;
-          col2.info.mtv = mtv.neg();
-
-          if (this.isResolver && !col1.rules.phantom && !col2.rules.phantom) {
-            const mtvHalf = mtv.cpy().mul(0.5);
-            t1.pos.add(mtvHalf);
-            t2.pos.sub(mtvHalf);
-          }
+        if (this.isResolver && !col1.rules.phantom && !col2.rules.phantom) {
+          const mtvHalf = mtv.cpy().mul(0.8);
+          t1.pos.add(mtvHalf);
+          t2.pos.sub(mtvHalf);
         }
       }
     }
   };
+
+  /**
+   * @param {Polygon2dCollider} col1
+   * @param {Transform2d} t1
+   * @param {Polygon2dCollider} col2
+   * @param {Transform2d} t2
+   */
+  calcCollision(col1, t1, col2, t2) {
+    if (
+      ((col1.rules.mask & col2.rules.layer) === 0 &&
+        (col2.rules.mask & col1.rules.layer) === 0) ||
+      !testBoundsBounds(col1.calcAabb(t1), col2.calcAabb(t2))
+    )
+      return null;
+
+    return calcMinTranslationVec(
+      col2.calcTransformed(t2),
+      col1.calcTransformed(t1),
+    );
+  }
 }
