7c7c4c976ce03fe7cb966c6ef0a74d194425aec8
cyjico <going2email2@gmail.com>
2024-11-15 11:06:37 +0800

    feat: add constructor to MovementComp

--- public/js/comps/movementComp.js
+++ public/js/comps/movementComp.js
@@ -1,12 +1,23 @@
-import Vector2d from "../ecs/util/vector2d.js";
+import Vector2d from '../ecs/util/vector2d.js';
 
 export default class MovementComp {
-  spd = 0.4;
-  smoothness = 0.01;
-  dir = Vector2d.zero;
-  targetDir = Vector2d.zero;
-  rotSpeed = 0.004;
-  rotSmoothness = 0.02;
-  rot = 0;
-  targetRot = 0;
+  constructor({
+    spd = 0.4,
+    smoothness = 0.01,
+    dir = Vector2d.zero,
+    targetDir = Vector2d.zero,
+    rotSpeed = 0.004,
+    rotSmoothness = 0.02,
+    rot = 0,
+    targetRot = 0,
+  } = {}) {
+    this.spd = spd;
+    this.smoothness = smoothness;
+    this.dir = dir;
+    this.targetDir = targetDir;
+    this.rotSpeed = rotSpeed;
+    this.rotSmoothness = rotSmoothness;
+    this.rot = rot;
+    this.targetRot = targetRot;
+  }
 }
