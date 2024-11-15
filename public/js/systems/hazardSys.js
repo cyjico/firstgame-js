644ce33a418b79b77f08a957fffe53a6b7c0b45e
cyjico <going2email2@gmail.com>
2024-11-15 18:10:22 +0800

    refactor: HazardSys listens to collision events

--- public/js/systems/hazardSys.js
+++ public/js/systems/hazardSys.js
@@ -1,29 +1,55 @@
 import HazardComp from '../comps/hazardComp.js';
 import HealthComp from '../comps/healthComp.js';
-import Polygon2dCollider, {
-  CollisionState,
-} from '../ecs/comps/polygon2dCollider.js';
+import Polygon2dCollider from '../ecs/comps/polygon2dCollider.js';
 import Sys from '../ecs/core/sys.js';
 
 export default class HazardSys extends Sys {
   /** @type {import("../ecs/core/sys.js").SysAction} */
-  fixedUpdate = ({ entMger }) => {
-    for (const ent of entMger.getEntsWithComp_t(HazardComp)) {
-      const hazard = entMger.getComp_t(ent, HazardComp);
-      const poly2dCol = entMger.getComp_t(ent, Polygon2dCollider);
-      if (!hazard || !poly2dCol) return;
+  start = ({ evtBus, entMger }) => {
+    evtBus.on(
+      'collision_enter',
+      /** @type {import('../ecs/systems/polygon2dCollision.js').CollisionEventListener} */
+      ({ ent1, ent2 }) => {
+        const $ = this.#structurizeEntities(entMger, ent1, ent2);
+        if (!$) return;
 
-      if (
-        poly2dCol.info.state & CollisionState.ENTER &&
-        poly2dCol.info.otherEntId
-      ) {
-        const other_health = entMger.getComp_t(ent, HealthComp);
-        if (!other_health) return;
-
-        other_health.curHp -= hazard.dmg;
-        if (other_health.curHp <= 0)
-          entMger.destroyEnt(poly2dCol.info.otherEntId);
-      }
-    }
+        $.other.health.curHp -= $.self.hazard.dmg;
+        if ($.other.health.curHp <= 0) entMger.destroyEnt($.other_id);
+      },
+    );
   };
+
+  /**
+   * @param {import('../ecs/core/entMger.js').default} entMger
+   * @param {number} ent1
+   * @param {number} ent2
+   */
+  #structurizeEntities(entMger, ent1, ent2) {
+    let self = ent1;
+    let other = ent2;
+    let hazard = entMger.getComp_t(self, HazardComp);
+    if (!hazard) {
+      self = ent2;
+      other = ent1;
+
+      hazard = entMger.getComp_t(self, HazardComp);
+      if (!hazard) return null;
+    }
+
+    const poly2dCol = entMger.getComp_t(self, Polygon2dCollider);
+    const other_health = entMger.getComp_t(other, HealthComp);
+    if (!poly2dCol || !other_health) return null;
+
+    return {
+      self_id: self,
+      self: {
+        hazard,
+        poly2dCol,
+      },
+      other_id: other,
+      other: {
+        health: other_health,
+      },
+    };
+  }
 }
