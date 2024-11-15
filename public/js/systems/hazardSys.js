65fa729d1dc40c955898c995a9c678d259f76257
cyjico <going2email2@gmail.com>
2024-11-15 22:54:15 +0800

    feat: add event for when hazard gets applied

--- public/js/systems/hazardSys.js
+++ public/js/systems/hazardSys.js
@@ -2,6 +2,13 @@ import HazardComp from '../comps/hazardComp.js';
 import HealthComp from '../comps/healthComp.js';
 import Sys from '../ecs/core/sys.js';
 
+/**
+ * @typedef {(data: {
+ *   ent1: number,
+ *   ent2: number
+ * }) => void} HazardEventListener
+ */
+
 export default class HazardSys extends Sys {
   /** @type {import("../ecs/core/sys.js").SysAction} */
   start = ({ evtBus, entMger }) => {
@@ -9,8 +16,14 @@ export default class HazardSys extends Sys {
       'collision_enter',
       /** @type {import('../ecs/systems/polygon2dCollision.js').CollisionEventListener} */
       ({ ent1, ent2 }) => {
-        this.#applyHazard(entMger, ent1, ent2);
-        this.#applyHazard(entMger, ent2, ent1);
+        if (
+          this.#applyHazard(entMger, ent1, ent2) ||
+          this.#applyHazard(entMger, ent2, ent1)
+        )
+          evtBus.emit('apply_hazard', {
+            ent1,
+            ent2,
+          });
       },
     );
   };
@@ -23,14 +36,14 @@ export default class HazardSys extends Sys {
   #applyHazard(entMger, selfId, otherId) {
     const hazard = entMger.getComp_t(selfId, HazardComp);
     const other_health = entMger.getComp_t(otherId, HealthComp);
-    if (!hazard || !other_health) return;
+    if (!hazard || !other_health) return false;
 
     const other_hazard = entMger.getComp_t(otherId, HazardComp);
-    if (other_hazard && hazard.excludeTags.has(other_hazard.tag)) return;
+    if (other_hazard && hazard.excludeTags.has(other_hazard.tag)) return false;
 
     other_health.curHp -= hazard.dmg;
-    if (other_health.curHp <= 0) {
-      entMger.destroyEnt(otherId);
-    }
+
+    if (other_health.curHp <= 0) entMger.destroyEnt(otherId);
+    return true;
   }
 }
